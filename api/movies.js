c9319c41da56807fd3d44ee7cf677c611721f281
benedikuning-a11y <benedik.uning@student.citrakasih.sch.id>
2026-05-30 21:52:11 +0800

    Update

--- api/movies.js
+++ api/movies.js
@@ -1,12 +1,18 @@
 export default async function handler(req, res) {
-  const response = await fetch(
-    `https://serpapi.com/search.json?engine=google_play_movies&q=Marvel&gl=us&hl=en&api_key=b5cb1f9ca1223d6c504064df322a3aeb4e1d2d3ce2e093a42c93b75cccf6954e`
-  );
+  try {
+    const response = await fetch(
+      `https://serpapi.com/search.json?engine=google_play_movies&q=Marvel&gl=us&hl=en&api_key=b5cb1f9ca1223d6c504064df322a3aeb4e1d2d3ce2e093a42c93b75cccf6954e`
+    );
 
-  const data = await response.json();
+    const data = await response.json();
 
-  res.status(200).json(data);
-  catch (error) {
-  res.status(500).json({ error: "Failed to fetch movies" });
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Server error",
+      error: String(error),
+    });
   }
 }
